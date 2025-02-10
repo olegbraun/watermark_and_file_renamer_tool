e37f9b96714b7fb8234394ab195ac4eb33fe4b30
Браун Олег Вадимович <opokotaev@gmail.com>
2025-02-10 17:12:07 +0300

    added random

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import os
 from glob import glob
+import random
 
 from watermark import add_watermark
 
@@ -20,14 +21,18 @@ for jpg_file in jpg_files:
 
 # Рекурсивно ищем все файлы с нужными расширениями
 for ext in EXTENSIONS:
-    files = glob(os.path.join(WORK_FOLDER, "*" + ext), recursive=True)
+    files = glob(os.path.join(WORK_FOLDER, "**/*" + ext), recursive=True)
     # Переименовываем каждый файл
     for file_path in files:
         try:
             # Получаем директорию и имя файла
-            dir_name, file_name = os.path.split(file_path)    
+            dir_name, file_name = os.path.split(file_path)
+            # Разделяем имя файла и его расширение
+            name_without_extension, extension = os.path.splitext(file_name)         
+            # Генерация уникального числа из 6 цифр
+            unique_number = str(random.randint(100000, 999999))
             # Новое имя файла
-            new_name = f"{TEXT_TO_FILES}_{file_name}"
+            new_name = f"{TEXT_TO_FILES}_{unique_number}{extension}"
             new_path = os.path.join(dir_name, new_name)
             # Переименовываем файл
             os.rename(file_path, new_path)
